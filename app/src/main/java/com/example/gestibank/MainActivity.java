98e9455669fea608b1601fa004045658133325b9
Wendong.Li <wendongli@meiway.com>
2021-03-01 13:34:57 +0100

    go to connect activity: done

--- app/src/main/java/com/example/gestibank/MainActivity.java
+++ app/src/main/java/com/example/gestibank/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.gestibank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button connect, createAcc, convertDevise;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connect = (Button) findViewById(R.id.btnGoToAuth);
+        createAcc = (Button) findViewById(R.id.btnCreateAccount);
+        convertDevise = (Button) findViewById(R.id.btnGoToConv);
+    }
+
+    public void goToAuthPage(View v){
+        Intent i = new Intent(getApplicationContext(), AuthActivity.class);
+        startActivity(i);
     }
 }
